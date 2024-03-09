1849baf5117b0f7df0e689814615d740803805f3
vdeekshithkumar <DeekshithK@ivoyant.com>
2024-03-09 17:21:12 +0530

    upload json file to blob storage

---
+++ BSB-project/BSB-project/Business/User2Business.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using BSB_project.Model;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace BSB_project.Business
+{
+    public class User2Business
+    {
+        private readonly string connectionString;
+
+        public User2Business(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public async Task<bool> UploadUserJsonAsync(Stream dataStream, string containerName, string blobName)
+        {
+            try
+            {
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+                BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+                await blobClient.UploadAsync(dataStream, true);
+                Console.WriteLine($"Data uploaded successfully to blob storage.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error uploading data to blob storage: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
+
+
